19de052ccd8c34f7b0f5b29bf970dc607972b14a
aryan-marthak <aryan.marthak1@gmail.com>
2024-01-26 01:32:52 +0530

    done

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-var btntranslate = document.querySelector("#btn-translate")
-var txtInput = document.querySelector("#txt-input")
-var outputDiv = document.querySelector("#output")
+var btnTranslate = document.querySelector("#btn-translate"); 
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+}
+
+// function errorHandler(error){
+//     console.log("error occured", error)
+//     alert("something wrong with server")
+// }
 
 
-console.log(txtInput);
+function clickHandler(){
+    var inputText = txtInput.value //taking input
 
-function clickhandler(){
-    outputDiv.innerText = "asjsoanveo " + txtInput.value
-};
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        // .catch(errorHandler)
+}
 
-
-btntranslate.addEventListener("click", clickhandler)
+btnTranslate.addEventListener("click", clickHandler)
